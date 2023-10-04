00dd70b446a31bc3456723ca9ced2743d1d4f10f
Fernando Rocha <frochaolivera18@hotmail.com>
2023-10-03 22:56:30 -0500

    feat: add header to controllers

--- src/authentication/presentation/auth-controller.ts
+++ src/authentication/presentation/auth-controller.ts
@@ -1,10 +1,16 @@
 import { Request, Response } from "express";
 
 export class AuthController {
+  //@desc Login user
+  //@route POST /api/auth/login
+  //@access public
   loginUser = (req: Request, res: Response) => {
     return res.status(200).json({ body: "Login Works!" });
   };
 
+  //@desc Register user
+  //@route POST /api/auth/register
+  //@access public
   registerUser = (req: Request, res: Response) => {
     return res.status(200).json({ body: "Register Works!" });
   };
