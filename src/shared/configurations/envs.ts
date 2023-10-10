7bbe9d0216cea0c3103a70d28c2ee0adc98b7152
Fernando Rocha <frochaolivera18@hotmail.com>
2023-10-09 22:17:54 -0500

    feat: add jwt configuration as an adapter

--- src/shared/configurations/envs.ts
+++ src/shared/configurations/envs.ts
@@ -5,4 +5,5 @@ export const envs = {
   PORT: get("PORT").required().asPortNumber(),
   MONGO_DB_NAME: get("MONGO_DB_NAME").required().asString(),
   MONGO_URL: get("MONGO_URL").required().asString(),
+  JWT_SEED: get("JWT_SEED").required().asString(),
 };
